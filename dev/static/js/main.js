26cfa356a5227f11569838151ea5871bb77d5208
Bogdan Lameko <bogdan.lmk@gmail.com>
2018-04-18 17:55:41 +0300

    new

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     svg4everybody({});
 });
+
+var navigation = $(".main-nav");
+    stickyNav = "stickyNav";
+    header = $('.header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > header ) {
+    navigation.addClass(stickyNav);
+  } else {
+    navigation.removeClass(stickyNav);
+  }
+});
