952ea6c7560a4ca376e380daf94d614ae7afd4d0
Bogdan Lameko <bogdan.lmk@gmail.com>
2018-04-18 23:42:02 +0300

    changes

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,14 +2,27 @@ $(document).ready(function () {
     svg4everybody({});
 });
 
-var navigation = $(".main-nav");
-    stickyNav = "stickyNav";
-    header = $('.header').height();
+// var navigation = $(".main-nav");
+//     stickyNav = "stickyNav";
+//     header = $('.header').height();
+//
+// $(window).scroll(function() {
+//   if( $(this).scrollTop() > header ) {
+//     navigation.addClass(stickyNav);
+//   } else {
+//     navigation.removeClass(stickyNav);
+//   }
+// });
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > header ) {
-    navigation.addClass(stickyNav);
-  } else {
-    navigation.removeClass(stickyNav);
-  }
-});
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    icon: '',
+    map: map
+  });
+}
